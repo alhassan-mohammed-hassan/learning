961c816b3270f54416a0151f2fba68a92fccc341
alHassan-Mohammed-hassan <hassanmohame209@icloud.com>
2026-09-06 15:50:10 +0300

    adding project

---
+++ project 1.py
@@ -0,0 +1,9 @@
+print("---age in dayes caculator---")
+age = int(input("please enter your age : "))
+months = age*12
+print(f"you lave lived for {months} month.")
+days = age *365 
+print(f"you lave lived for {days} days.")
+hours = days*24
+print(f"you lave lived for {hours} hours.")
+
